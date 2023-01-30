8a593566928b3a1e81c163f417056fc3e383d038
luisao23c <luisao23c3@gmail.com>
2023-01-30 09:57:10 -0800

    cambio

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-let array_imgs = [];
+let array_imgs = null;
 let cont_imgs = 0;
 let preview = null;
 let add = document.getElementById("add");
 add.addEventListener("click",(e)=>{
+    cont_imgs ++;
     let input = document.createElement("input");
     input.type = "file";
-    input.id = "preview" + cont_img;
+    input.id = "preview" + cont_imgs;
+    input.class = "selected";
+     array_imgs = document.querySelectorAll('.selected');
     container.appendChild(input);
     input = document.createElement("img");
-    input.id = "img";
+    input.id = "img" + cont_imgs;
     input.src ="#";
     input.alt= "tu imagen";
     container.appendChild(input);
-     setTimeout(function(){
-        preview = document.getElementById("preview");
+        setTimeout(function(){
+        preview = document.getElementById("preview" + cont_imgs);
         showPreview();
-    }, 2000);
-  
+    }, 5000);
+    
      container.appendChild(document.createElement("br"));
 });
 function showPreview(){
         const [file] = preview.files
-            document.getElementById("img").src = URL.createObjectURL(file);
+        document.getElementById("img" + cont_imgs).src = URL.createObjectURL(file);
 }
 
