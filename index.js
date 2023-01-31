28197bde280564a68976442be971c7f41136e48e
luisao23c <luisao23c3@gmail.com>
2023-01-31 07:45:28 -0800

    c2

--- index.js
+++ index.js
@@ -5,30 +5,35 @@ let preview = null;
 let add = document.getElementById("add");
 add.addEventListener("click",(e)=>{
     if(cont_imgs > 0){
-        if(img.src == "http://127.0.0.1:5500/#")
+        if(img.src == "http://127.0.0.1:5500/index.html#")
         {
             return alert("No se agrego una imagen por favor ingresera para poder agregar mas imagenes");
         }
     }
     cont_imgs ++;
-     input = document.createElement("input");
-    input.type = "file";
-    input.id = "preview" + cont_imgs;
-    
-    input.onchange= function() {showPreview()};
-    input.class = "boton-standar-rw:hover";
-    button.appendChild(input);
+    let div_button = document.createElement("div");
+    div_button.classList.add("col-3");
+    div_button.classList.add("offset-3");
+    container.appendChild(div_button);
+    let div_description = document.createElement("div");
+    div_description.classList.add("col-3");
+    container.appendChild(div_description);
     img = document.createElement("img");
     img.id = "img" + cont_imgs;
     img.src ="#";
     img.alt= "tu imagen";
-    imagen.appendChild(img);
-    container.appendChild(document.createElement("br"));
+    div_button.appendChild(img);
+    div_button.appendChild(document.createElement("br"));
+     input = document.createElement("input");
+    input.type = "file";
+    input.id = "preview" + cont_imgs;
+    input.onchange= function() {showPreview()};
+    div_button.appendChild(input);
     input = document.createElement("input");
     input.placeholder = "Ingresa una descripción";
     input.type = "text";
-    description.appendChild(input);
-
+    div_description.appendChild(input);
+    container.appendChild(document.createElement("br"));
 });
 function showPreview(){   
     preview = document.getElementById("preview" + cont_imgs);
